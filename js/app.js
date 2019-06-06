d5aa9f96709b6ece87ac9ae94134aa6a26ba59db
Eric Huang <ejhuang84@gmail.com>
2019-06-06 15:11:53 -0700

    added mac functionality to html, updated questions 1 - 4 with functions

--- js/app.js
+++ js/app.js
@@ -1,79 +1,84 @@
 // test = window.confirm("yes or no?");
 // console.log(test);
-'use strict'
+'use strict';
 
-
-var input = prompt("Would you like to play a game with me? Answer yes or no.");
-
-// change the user input to all uppercase then split the string into an array filled with every characters and then take the first character ot test for y or n for yes or no 
-var test = input.toUpperCase();
-test = test.split("");
-var firstLetter = test[0]; 
+//function takes input prompt, converts to uppercase and puts into an array test and returns the first index of array.
+var inputCheck = function (input) {
+  var test = input.toUpperCase();
+  test = test.split("");
+  var firstLetter = test[0];
+  return firstLetter;
+};
 
 // keep track of how many answer user got right
 var score = 0;
 
-// test user answer and execute the case for the corresponding answer.
-if (firstLetter === "N") { 
-  alert("Well, you are missing out on some seriously fun");
-} else { 
-  alert("Answer yes or no for each question about me. Let see if you can guess right. For each right answer, you get a different prize.")
-
-  // repeat the process for each question
-  input = prompt("Is my last name Nguyen, the most common Vietnamese' last name ever?");
-  test = input.toUpperCase();
-  test = test.split("");
-  firstLetter = test[0];
-
-  if (firstLetter === "Y") { 
+//question functions. takes prompt, runs inputCheck provide feedback.
+var q1 = function (){
+  input = inputCheck(prompt("Is my last name Nguyen, the most common Vietnamese' last name ever?"));
+  if (input === "Y") { 
     score++;
     alert("Correct. The odds were in your favor though, like 80%.");
   } else { 
     alert("Incorrect. And also incorrect if you think I am related to a Nguyen you know.")
   }
-
-  input = prompt("Am I the sexiest person you know?");
-  test = input.toUpperCase();
-  test = test.split("");
-  firstLetter = test[0];
-
-  if (firstLetter === "Y") { 
+}
+var q2 = function() {
+  input = inputCheck(prompt("Am I the sexiest person you know?"));
+  if (input === "Y") { 
     score++;
     alert("That's right. And don't you ever forget it.");
   } else { 
     alert("That's 100% wrong. You should get your eyes checked.");
   }
-
+}
+var q3 = function() {
   input = prompt("Was I once the champion of the Spelling Bee in elementary school?");
-  test = input.toUpperCase();
-  test = test.split("");
-  firstLetter = test[0];
-
-  if (firstLetter === "N") { 
+  if (input === "N") { 
     score++;
     alert("You are right. I was knocked out in the first round. It was indeed the saddest moment in human history.");
   } else { 
     alert("Thank you for thinking that I am smart that is incorrect. I should have won but unfortunately the competition was rigged.");
   }
-
+}
+var q4 = function() {
   input = prompt("Was I once the champion of my high school's talent show?")
-  test = input.toUpperCase();
-  test = test.split("");
-  firstLetter = test[0];
-
-  if (firstLetter === "Y") { 
+  if (input === "Y") { 
     score++;
     alert("That's absolutely right. I gave everyone quite a show. Every person that attended shed a tear and chanted my name.");
   } else { 
     alert("You are dead wrong. I was freaking awesome.");
   }
+}
+var q5
+var q6
+var q7
+
+var input = inputCheck(prompt("Would you like to play a game with me? Answer yes or no."));
+
+// change the user input to all uppercase then split the string into an array filled with every characters and then take the first character ot test for y or n for yes or no 
+// var test = input.toUpperCase();
+// test = test.split("");
+// var firstLetter = test[0]; 
+
+
+// test user answer and execute the case for the corresponding answer.
+if (input === "N") { 
+  alert("Well, you are missing out on some seriously fun");
+} else { 
+  alert("Answer yes or no for each question about me. Let see if you can guess right. For each right answer, you get a different prize.")
+
+  // repeat the process for each question
+  
+
+  
+
+  
 
   input = prompt("Did I ever kill anyone?");
-  test = input.toUpperCase();
-  test = test.split("");
-  firstLetter = test[0];
 
-  if (firstLetter === "Y") { 
+
+  if (input === "Y") { 
     alert("Yep I actually did once");
     alert("Just kidding. How could I do such a thing? I am a saint.");
   } else { 
